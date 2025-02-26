137a03e202b2531fd5d1e9f727714474422d9002
Iyisku <nofuiyisku@gmail.com>
2025-02-26 15:11:07 +0500

    minor changes

--- Personal_Portfolio/src/Pages/Experience.jsx
+++ Personal_Portfolio/src/Pages/Experience.jsx
@@ -1,36 +1,37 @@
 import React from "react";
-import { FaGoogle, FaMagento, FaTableTennis} from "react-icons/fa";
+import { FaGamepad, FaGoogle, FaMagento, FaTableTennis } from "react-icons/fa";
 
 export default function Experience() {
   const experiences = [
     {
-      role: "FUll Stack Web Developer at TECHXSERVE",
+      role: "Full Stack Web Developer at TECHXSERVE",
       company: "TechXServe",
-      icon: <FaTableTennis className="text-xl text-red-500" />,
+      icon: <FaTableTennis className="text-xl group-hover:text-black text-red-500" />,
       description:
         "As a Full stack Web Developer at TechXServe, I played a pivotal role in developing innovative solutions. Collaborating with a team of engineers, I contributed to more than 3 Projects and Completed Clients Requirements On Schedule.",
-      date: "August 2024  – Present",
-      Techused: 'Js, TailwindCss, Node.js, React.Js, Next.js, Express.js, MongoDB, Git, Github, Visual Studio, Figma, Redis',
+      date: "August 2024 – Present",
+      Techused:
+        "Js, TailwindCss, Node.js, React.Js, Next.js, Express.js, MongoDB, Git, Github, Visual Studio, Figma, Redis",
     },
     {
       role: "C# Developer at Metal Heart Games Studio",
       company: "Metal Heart Studio",
-      icon: <FaMagento className="text-xl text-red-500" />,
+      icon: <FaGamepad className="text-xl group-hover:text-black text-red-500" />,
       description:
-        "As a C# Developer with Unity at Metal Heart Studio, I played a pivotal role in developing Engaging Games. I contributed to more than 2 Projects and Learned Industy Best Practices In programming.",
-      date: "January 2024  – July 2024",
-      Techused: "Unity, C# , .Net,  Git, Github, Visual Studio, Figma",
+        "As a C# Developer with Unity at Metal Heart Studio, I played a pivotal role in developing Engaging Games. I contributed to more than 2 Projects and Learned Industry Best Practices In programming.",
+      date: "January 2024 – July 2024",
+      Techused: "Unity, C# , .Net, Git, Github, Visual Studio, Figma",
     },
     {
       role: "Intern Software Engineer at Arkaen Solutions",
       company: "Arkaen Solutions",
-      icon: <FaMagento className="text-xl text-red-500" />,
+      icon: <FaMagento className="text-xl group-hover:text-black text-red-500" />,
       description:
         "As a Software Engineer at Arkaen Solutions, I got to learn from the very basics of programming. Collaborating with Senior Developers, I got to work with different Technologies and guidance from the best of developers.",
       date: "July 2023 – January 2024",
-      Techused: 'Html, Css, Js, Go, Azure, C# , Git, Github, Visual Studio, Figma, Discord, Slack',
+      Techused:
+        "Html, Css, Js, Go, Azure, C# , Git, Github, Visual Studio, Figma, Discord, Slack",
     },
-   
   ];
 
   return (
@@ -45,20 +46,28 @@ export default function Experience() {
         {experiences.map((exp, index) => (
           <div
             key={index}
-            className="bg-[#000] p-6 rounded-lg shadow-lg flex flex-col md:flex-row justify-between border border-white hover:bg-gray-900 hover:cursor-pointer"
+            className="group bg-black p-8 rounded-lg flex flex-col md:flex-row justify-between 
+            border border-white transition duration-300 ease-in-out 
+            hover:bg-white hover:text-black hover:cursor-pointer shadow-lg shadow-white"
           >
             {/* Left Side - Icon & Role */}
             <div className="flex items-center space-x-4">
-              <div className="p-2 bg-gray-800 rounded-full">{exp.icon}</div>
+              <div className="p-2 bg-gray-800 rounded-full group-hover:bg-white">
+                {exp.icon}
+              </div>
               <div>
-                <h2 className="text-lg font-bold">{exp.role}</h2>
-                <p className="text-gray-400 text-sm mt-2">{exp.description}</p>
-                <p className="text-gray-300 text-sm mt-2">Technologies Used: {exp.Techused}</p>    
+                <h2 className="text-lg font-bold group-hover:text-black">{exp.role}</h2>
+                <p className="text-gray-400 text-sm mt-2 group-hover:text-black">
+                  {exp.description}
+                </p>
+                <p className="text-gray-300 text-sm mt-2 group-hover:text-black">
+                  <strong>Technologies Used:</strong> {exp.Techused}
+                </p>
               </div>
             </div>
 
             {/* Right Side - Date */}
-            <p className="text-gray-300 text-sm md:text-right mt-4 md:mt-0 whitespace-nowrap">
+            <p className="text-gray-300 text-sm md:text-right mt-4 md:mt-0 whitespace-nowrap group-hover:text-black">
               {exp.date}
             </p>
           </div>
--- Personal_Portfolio/src/Pages/ProjectsPage.jsx
+++ Personal_Portfolio/src/Pages/ProjectsPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ProjectsPage() {
   return (
-    <div>ProjectsPage</div>
+    <div>
+      <h1>Projects</h1>
+    </div>
   )
 }
